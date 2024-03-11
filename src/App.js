9d29639ffc65c546d65cbaa4d9d995823dcdea8d
Jhon-Crow <jhamish@mail.ru>
2024-03-11 07:09:42 +0300

    1h 15min
    не удалось сделать сортировку как на видео, но возможно сработает так

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ import Postlist from "./components/Postlist";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([ //передаём массив объектов
 
     ])
 
+
+    const [selectedSort, setSelectedSort] = useState('')
+
+    function getSortedPosts(selectedSort){
+        if(selectedSort){
+            console.log('selectedSort')
+            return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort()]))
+        }
+        return posts;
+    }
+
+    const sortedPosts = getSortedPosts()
     const createPost = (newPost)=> {
         setPosts([...posts, newPost])
     }
@@ -21,23 +34,51 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort]))) //мутируем копию, сравниваем строки
+    }
+
+    const [searchQuery, setSearchQuery] = useState('')
+
   return (
     <div className="App">
-
-
-
-
 
         {/*<Postlist posts={posts} title='title TEST 1'/>*/}
         {/*<Postlist posts={posts2} title='title TEST 2'/>*/}
 
 
         <PostForm create={createPost}/>
-        <h1 style={{textAlign: "center"}}>
-            Список постов
-        </h1>
 
-        <Postlist remove={removePost} posts={posts}/>
+        <hr style={{margin: '15px 0'}}/>
+        <div>
+            <MyInput
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                placeholder={'Search'}
+            />
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+            defaultValue='Sort by...'
+            option={[
+                {value:'title', name: 'By name'},
+                {value:'body', name: 'By discription'}
+            ]}
+            />
+            {/*<select>*/}
+            {/*    <option value={'value1'}>По описанию</option>*/}
+            {/*    <option value={'value2'}>По названию</option>*/}
+            {/*</select>*/}
+        </div>
+
+        {posts.length
+            ? <Postlist remove={removePost} posts={sortedPosts} title={'Список постов'}/>
+            : <h1 style={{textAlign: 'center'}}>
+                Posts not finde
+        </h1>
+        }
+        {/*<Postlist remove={removePost} posts={posts}/>*/}
 
         {/*берём массив posts, через мап обращаемся к каждому элементу и перобразуем в реакт элемент*/}
         {/*{posts.map(post =>*/}
