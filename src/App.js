76bcbd0ff91904c49accad1638ba1aca6dd3839b
Jhon-Crow <jhamish@mail.ru>
2024-03-18 20:30:05 +0300

    2h 15 min

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import React, {useEffect, useMemo, useRef, useState} from 'react';
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
-import './styles/app.css'
-import PostItem from "./components/PostItem";
-import Postlist from "./components/Postlist";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import PostFilter from "./components/PostFilter.jsx";
-import {usePosts} from "./hooks/usePosts.js";
-import axios from "axios";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/Loader/Loader";
-import {useFetching} from "./hooks/useFetching.js";
-import {getPageCount, getPagesArray} from "./utils/pages.js";
-
-// import * as querystring from "querystring";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
-  return (
-      <div>
-          Working
-      </div>
-  )
+    return (
+        <BrowserRouter>
+            <Route path='/about'>
+                <About/>
+            </Route>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            срисовал всё из видео
+        </h1>
+    );
+};
+
+export default About;
